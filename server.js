7bfb6ab1d41dd40454fb39fc3b52cffdba35b6b6
SERDOTTE <rodrigo.serdotte@gmail.com>
2026-03-21 23:59:59 -0300

    10 - w003 learning ativity 3/3

--- server.js
+++ server.js
@@ -21,10 +21,7 @@ app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
 
-
 app.use(static)
-
-
 
 /* ***********************
  * Local Server Information
@@ -33,17 +30,10 @@ app.use(static)
 const port = process.env.PORT || 5500
 const host = process.env.HOST || "localhost"
 
-/* ***********************
- * Log statement to confirm server operation
- *************************/
-app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
-
 //Index route
-app.get("/", baseController.buildHome)
+app.get("/", utilities.handleErrors(baseController.buildHome))
 
-app.get("/custom", async function(req, res) {
+app.get("/custom", utilities.handleErrors(async function(req, res) {
   const nav = await utilities.getNav()
   res.render("index", { title: "Custom", nav })
 })
@@ -65,3 +55,30 @@ app.get("/truck", async function(req, res) {
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+/* ***********************
+ * Log statement to confirm server operation
+ *************************/
+app.listen(port, () => {
+  console.log(`app listening on ${host}:${port}`)
+})
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
